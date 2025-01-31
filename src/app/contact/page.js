caa480653e2193c7aed84140a9abb8260940cb43
preettypeanuts <mrafly212001@gmail.com>
2025-01-31 15:36:15 +0700

    added contact list

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,4 +1,5 @@
 import { Banner } from "@/components/Banner";
+import { ContactList } from "@/components/ContactList";
 
 export default function Contact() {
     return (
@@ -7,6 +8,7 @@ export default function Contact() {
                 head={<>Hubungi Kami!</>}
                 imgUrl={'https://images.unsplash.com/photo-1703669020883-66f3e77ae929?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
             />
+            <ContactList />
         </>
     )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,27 @@
+import { dataContact } from "../../public/System"
+
 export const ContactList = () => {
     return (
         <>
-        <section>
-            
-        </section>
+            <section className="md:mx-10 mx-5">
+                <div className="flex flex-wrap gap-3">
+                    {[...dataContact.mail, ...dataContact.phone, ...dataContact.media].map((el, idx) => (
+                        <a
+                            key={idx}
+                            href=""
+                            className="w-fit h-[15lvh] p-5 flex flex-col items-start grow justify-between dark:bg-darkColor bg-mainColorD rounded-2xl"
+                        >
+                            <div className="flex items-center tracking-wide text-xs font-medium uppercase px-2 py-[2px] bg-secondaryColor bg-opacity-70 rounded-full gap-2 w-fit">
+                                {el.icon}
+                                {el.label}
+                            </div>
+                            <div className="font-medium">
+                                {el.data}
+                            </div>
+                        </a>
+                    ))}
+                </div>
+            </section>
         </>
     )
 }
